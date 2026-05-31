4295f38b6654f8e95c10d876e06aaf8a48a58330
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-05-31 16:33:08 +0200

    Capitalize words

--- repl_test.go
+++ repl_test.go
@@ -6,13 +6,30 @@ import (
 )
 
 type Test struct {
+	info	string
 	input	string
 	want	[]string
 }
 
 var tests = []Test{
 	{
+		info:		"One word",
 		input: 	"Pikachu",
+		want:		[]string{"Pikachu"},
+	},
+	{
+		info:		"Two words",
+		input:	"Pikachu Ditto",
+		want:		[]string{"Pikachu", "Ditto"},
+	},
+	{
+		info:		"Empty string",
+		input:	"",
+		want:		[]string{},
+	},
+	{	
+		info:		"All uppercase",
+		input:	"PIKACHU",
 		want:		[]string{"Pikachu"},
 	},
 }
@@ -21,7 +38,7 @@ func TestCleanInput(t *testing.T) {
 	for _, test := range tests {
 		got := cleanInput(test.input)
 		if !slices.Equal(got, test.want) {
-			t.Errorf("got %v, want %v\n", got, test.want)
+			t.Errorf("%q: got %v, want %v\n", test.info, got, test.want)
 		}
 	}
 }
