4ed8a251edf9337f27238eb75eef236ce53af2ea
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-04 14:52:20 +0200

    Parse more attributes for Pokemon struct

--- internal/commands/structs.go
+++ internal/commands/structs.go
@@ -11,7 +11,7 @@ type CliCommand struct {
 	callback func(pokedex map[string]Pokemon, config *config.Config, cache *cache.Cache, argument string) error
 }
 
-// Structures to store json return from "https://pokeapi.co/api/v2/location-area/{name}"
+// Structures to store json returned from "https://pokeapi.co/api/v2/location-area/{name}"
 type Location struct {
 	Name				string			`json:"name"`
 	Encounters	[]Encounter `json:"pokemon_encounters"`	
@@ -21,9 +21,32 @@ type Encounter struct {
 	Pokemon	Pokemon	`json:"pokemon"`
 }
 
+// Structures to store json returned from "https://pokeapi.co/api/v2/pokemon/{name}"
+
 type Pokemon struct {
-	Name	string	`json:"name"`
-	Exp		int			`json:"base_experience"`
+	Name		string	`json:"name"`
+	Exp			int			`json:"base_experience"`
+	Height	int			`json:"height"`
+	Weight	int			`json:"weight"`
+	Stats		[]Stats	`json:"stats"`
+	Types		[]Types `json:"types"`
+}
+
+type Stats struct {
+	BaseStat	int		`json:"base_stat"`
+	Stat			Stat	`json:"stat"`
+}
+
+type Stat	struct {
+	Name	string `json:"name"`
+}
+
+type Types struct {
+	Type	Type	`json:"type"`
+}
+
+type Type struct {
+	Name	string `json:"name"`
 }
 
 // Structures to store json returned from "https://pokeapi.co/api/v2/location-area"
